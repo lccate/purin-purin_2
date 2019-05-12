0aeea7bec1c84041dae69f7a1872919777671809
lccate <lcccate@163.com>
2019-05-12 15:27:40 +0800

    Add files via upload

---
+++ NULL_pointer.cpp
@@ -0,0 +1,20 @@
+//文件名：NULL_pointer
+//作  者：ccate
+//时  间：2018.12
+//描  述：空指针与野指针
+
+#include<stdio.h>
+
+int main03()
+{
+	/*野指针
+	int *p;
+	*p = 100;//没有指定内存
+	*/
+
+	//空指针
+	int *p1;
+	p1 = NULL;
+
+	return 0;
+}
